6ce471d648bbb1d6509ee13288e7aaa7bf32a207
Gabriel Bojikian <gabriel.f.r.bojikian@gmail.com>
2021-01-26 22:42:43 -0300

    End of lesson 2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Head from 'next/head';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,15 +21,11 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  let [name, setName] = React.useState('');
+
   return (
     <>
-      <Head>
-        <title>{db.title}</title>
-        <meta property="og:title" content={db.title} />
-        <meta property="og:site_name" content={db.title} />
-        <meta property="og:image" content={db.bg} key="image" />
-        <meta property="og:description" content="Teste seus conhecimentos sobre boxe" />
-      </Head>
       <QuizBackground backgroundImage={db.bg}>
         <QuizContainer>
           <QuizLogo />
@@ -36,7 +34,29 @@ export default function Home() {
               <h1>{db.title}</h1>
             </Widget.Header>
             <Widget.Content>
-              <p>{db.description}</p>
+              <form onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+
+                console.log('Fazendo uma submissão por meio do react');
+
+                // Router manda para a próxima página
+              }}
+              >
+                <input
+                  onChange={function (infosDoEvento) {
+                    // State
+                    // name = infosDoEvento.target.value;
+                    setName(infosDoEvento.target.value);
+                  }}
+                  placeholder="Digite seu nome"
+                />
+                <button type="submit" disabled={name.length === 0}>
+                  Jogar
+                  {' '}
+                  {name}
+                </button>
+              </form>
             </Widget.Content>
           </Widget>
 
